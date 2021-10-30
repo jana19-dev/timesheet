0e7adb203c928fb86ef11e1fe918964e0bf39268
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-10-30 00:08:41 -0400

    Update readme and homepage

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Time Sheet
+# <img src="public/logo.png" alt="drawing" width="30"/> Timesheet
+
+## A simple app to keep track of work hours and estimate payout
+
+- Keep track of work hours
+- Show estimated bi-weekly payouts
+- Show visual representation of work hours
+- Show visual representation of estimated payout
+
+**Built using Svelte and deployed using Netlify**
--- src/App.svelte
+++ src/App.svelte
@@ -3,5 +3,32 @@
 </script>
 
 <main>
-	<h1>Hello!</h1>
+	<header>
+		<img src="logo.png" alt="logo"/>
+		<h1>Timesheet</h1>
+	</header>
 </main>
+
+<style>
+	main {
+		height: 100vh;
+		overflow-y: auto;
+	}
+
+	header {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	img {
+		width: 40px;
+		height: 40px;
+		padding-right: 0.5rem;
+	}
+
+	h1 {
+		font-size: 2em;
+		margin: 0;
+	}
+</style>
